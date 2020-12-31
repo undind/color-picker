405eb153213f7955b67ce7bcf96f9ade31947cf3
undind <flyker777@yandex.ru>
2020-12-31 21:47:02 +0300

    Clear code

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
-export { default as getAlphaHex } from './getAlphaHex';
 export { default as hexAlphaToRgba } from './hexAlphaToRgba';
 export { default as hexToRgba } from './hexToRgba';
 export { default as getHexAlpha } from './getHexAlpha';
